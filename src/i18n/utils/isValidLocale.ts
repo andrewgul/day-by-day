48ebf0fc306103171571f4b933e079ada7e9c871
Andrew Gulin <a.gulin@ktsstudio.ru>
2026-01-28 03:33:07 +0300

    DAY-5 test commit

--- src/i18n/utils/isValidLocale.ts
+++ src/i18n/utils/isValidLocale.ts
@@ -1,6 +1,8 @@
 import { Locale } from "next-intl";
 import { routing } from "../routing";
 
+// test
+
 export const isValidLocale = (value?: string | null): value is Locale => {
   if (!value) {
     return false;
